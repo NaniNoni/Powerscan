5a67f95163a9f7b0a713d277f8b98531ac999cb5
NaniNoni <michalwaligora62@proton.me>
2025-08-31 11:02:30 +0200

    add gtk hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, glib};
+use gtk4 as gtk;
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("com.github.naninoni.powerscan")
+        .build();
+
+    app.connect_activate(|app| {
+        // We create the main window.
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("Hello, World!")
+            .build();
+
+        // Show the window.
+        window.present();
+    });
+
+    app.run()
 }
