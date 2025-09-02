85eeebf340d8e550bb0adadf7ff8ee6884e108c6
NaniNoni <michalwaligora62@proton.me>
2025-09-02 20:08:12 +0200

    initialize sane asynchronously

--- powerscan/src/main.rs
+++ powerscan/src/main.rs
@@ -1,7 +1,16 @@
+use std::sync::OnceLock;
+
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, glib};
 use gtk4 as gtk;
-use sane::Sane;
+use gtk4::glib::clone;
+use sane::{Device, Sane, SaneError};
+use tokio::runtime::Runtime;
+
+fn tokio_runtime() -> &'static Runtime {
+    static RUNTIME: OnceLock<Runtime> = OnceLock::new();
+    RUNTIME.get_or_init(|| Runtime::new().expect("Setting up tokio runtime needs to succeed."))
+}
 
 fn main() -> glib::ExitCode {
     let app = Application::builder()
@@ -20,12 +29,33 @@ fn main() -> glib::ExitCode {
         // Show the window.
         window.present();
 
-        let sane = Sane::init(0).unwrap();
-        let device_list = sane.get_devices().unwrap();
-        for device in device_list {
-            println!("{:?}", device);
-        }
-        let handle = sane.open("LiDE 220");
+        let (sender, receiver) = async_channel::bounded::<Result<Vec<Device>, SaneError>>(1);
+        tokio_runtime().spawn(clone!(
+            #[strong]
+            sender,
+            async move {
+                let device_list = Sane::init(0).map_or_else(Err, |sane| sane.get_devices());
+                sender
+                    .send(device_list)
+                    .await
+                    .expect("Failed to open channel");
+            }
+        ));
+
+        glib::spawn_future_local(async move {
+            while let Ok(device_list) = receiver.recv().await {
+                match device_list {
+                    Ok(device_list) => {
+                        for device in device_list {
+                            println!("{:?}", device);
+                        }
+                    }
+                    Err(e) => {
+                        eprintln! {"{e}"};
+                    }
+                }
+            }
+        });
     });
 
     app.run()
