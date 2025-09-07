59ef936d558ccb4266ff12ce573dbfdabb44bed9
NaniNoni <michalwaligora62@proton.me>
2025-09-07 19:08:42 +0200

    [powerscan] replace gtk4 with relm4 "Hello World" example

--- powerscan/src/main.rs
+++ powerscan/src/main.rs
@@ -1,73 +1,107 @@
-use std::sync::OnceLock;
+use gtk::glib::clone;
+use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt};
+use relm4::{ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent, gtk};
 
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, glib};
-use gtk4::glib::clone;
-use gtk4::{self as gtk, CenterBox, Spinner};
-use log::{error, info};
-use sane::{Device, Sane, SaneError};
-use tokio::runtime::Runtime;
-
-fn tokio_runtime() -> &'static Runtime {
-    static RUNTIME: OnceLock<Runtime> = OnceLock::new();
-    RUNTIME.get_or_init(|| Runtime::new().expect("Setting up tokio runtime needs to succeed."))
+struct AppModel {
+    counter: u8,
 }
 
-fn main() -> glib::ExitCode {
-    env_logger::init();
+#[derive(Debug)]
+enum AppMsg {
+    Increment,
+    Decrement,
+}
 
-    let app = Application::builder()
-        .application_id("com.github.naninoni.powerscan")
-        .build();
+struct AppWidgets {
+    label: gtk::Label,
+}
 
-    app.connect_activate(|app| {
-        let center_box = CenterBox::new();
-        let spinner = Spinner::new();
-        center_box.set_center_widget(Some(&spinner));
+impl SimpleComponent for AppModel {
+    /// The type of the messages that this component can receive.
+    type Input = AppMsg;
+    /// The type of the messages that this component can send.
+    type Output = ();
+    /// The type of data with which this component will be initialized.
+    type Init = u8;
+    /// The root GTK widget that this component will create.
+    type Root = gtk::Window;
+    /// A data structure that contains the widgets that you will need to update.
+    type Widgets = AppWidgets;
 
-        // We create the main window.
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .default_width(320)
-            .default_height(200)
-            .title("Powerscan")
-            .child(&center_box)
+    fn init_root() -> Self::Root {
+        gtk::Window::builder()
+            .title("Simple app")
+            .default_width(300)
+            .default_height(100)
+            .build()
+    }
+
+    /// Initialize the UI and model.
+    fn init(
+        counter: Self::Init,
+        window: Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> relm4::ComponentParts<Self> {
+        let model = AppModel { counter };
+
+        let vbox = gtk::Box::builder()
+            .orientation(gtk::Orientation::Vertical)
+            .spacing(5)
             .build();
 
-        // Show the window.
-        window.present();
+        let inc_button = gtk::Button::with_label("Increment");
+        let dec_button = gtk::Button::with_label("Decrement");
 
-        spinner.start();
-        let (sender, receiver) = async_channel::bounded::<Result<Vec<Device>, SaneError>>(1);
-        tokio_runtime().spawn(clone!(
+        let label = gtk::Label::new(Some(&format!("Counter: {}", model.counter)));
+        label.set_margin_all(5);
+
+        window.set_child(Some(&vbox));
+        vbox.set_margin_all(5);
+        vbox.append(&inc_button);
+        vbox.append(&dec_button);
+        vbox.append(&label);
+
+        inc_button.connect_clicked(clone!(
             #[strong]
             sender,
-            async move {
-                let device_list = Sane::init().map_or_else(Err, |sane| sane.get_devices());
-                sender
-                    .send(device_list)
-                    .await
-                    .expect("Failed to open channel");
+            move |_| {
+                sender.input(AppMsg::Increment);
             }
         ));
 
-        glib::spawn_future_local(async move {
-            while let Ok(device_list) = receiver.recv().await {
-                match device_list {
-                    Ok(device_list) => {
-                        for device in device_list {
-                            info!("{:?}", device);
-                        }
-                    }
-                    Err(e) => {
-                        error!("Error retreaving SANE device list: {:?}", e)
-                    }
-                }
-
-                spinner.stop();
+        dec_button.connect_clicked(clone!(
+            #[strong]
+            sender,
+            move |_| {
+                sender.input(AppMsg::Decrement);
             }
-        });
-    });
+        ));
 
-    app.run()
+        let widgets = AppWidgets { label };
+
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
+        match message {
+            AppMsg::Increment => {
+                self.counter = self.counter.wrapping_add(1);
+            }
+            AppMsg::Decrement => {
+                self.counter = self.counter.wrapping_sub(1);
+            }
+        }
+    }
+
+    /// Update the view to represent the updated model.
+    fn update_view(&self, widgets: &mut Self::Widgets, _sender: ComponentSender<Self>) {
+        widgets
+            .label
+            .set_label(&format!("Counter: {}", self.counter));
+    }
+}
+
+fn main() {
+    let app = RelmApp::new("relm4.test.simple_manual");
+    app.run::<AppModel>(0);
 }
