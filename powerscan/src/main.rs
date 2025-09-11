a20833c4685b679a712d9165cdd87949040c165b
NaniNoni <michalwaligora62@proton.me>
2025-09-11 19:33:04 +0200

    [powerscan] default libcosmic template
    Generated using the cargo-generate utility with `cargo generate
    gh:pop-os/cosmic-app-template`.

--- powerscan/src/main.rs
+++ powerscan/src/main.rs
@@ -1,90 +1,23 @@
-use iced::widget::{Button, button, container, scrollable, text};
-use iced::{Length, Size, Task, Theme, alignment};
+// SPDX-License-Identifier:  GPL-3.0-or-later
 
-use iced_aw::menu::{self, Item, Menu};
-use iced_aw::{menu_bar, menu_items};
+mod app;
+mod config;
+mod i18n;
 
-pub fn main() -> iced::Result {
-    env_logger::init();
+fn main() -> cosmic::iced::Result {
+    // Get the system's preferred languages.
+    let requested_languages = i18n_embed::DesktopLanguageRequester::requested_languages();
 
-    iced::application(App::title, App::update, App::view)
-        .theme(App::theme)
-        .font(iced_fonts::REQUIRED_FONT_BYTES)
-        .window_size(Size::new(1000.0, 600.0))
-        .run()
-}
+    // Enable localizations to be applied.
+    i18n::init(&requested_languages);
 
-#[derive(Debug, Clone)]
-enum Message {
-    /// Used to create a [`Task::none`].
-    /// This can be useful to make disabled buttons look like clickable ones.
-    None,
-    /// Message to exit the app
-    Quit,
-}
+    // Settings for configuring the application window and iced runtime.
+    let settings = cosmic::app::Settings::default().size_limits(
+        cosmic::iced::Limits::NONE
+            .min_width(360.0)
+            .min_height(180.0),
+    );
 
-struct App {
-    title: String,
-    theme: iced::Theme,
-}
-
-impl Default for App {
-    fn default() -> Self {
-        Self {
-            title: "Powerscan".to_string(),
-            theme: iced::Theme::Light,
-        }
-    }
-}
-
-impl App {
-    fn title(&self) -> String {
-        self.title.clone()
-    }
-
-    fn update(&mut self, message: Message) -> Task<Message> {
-        match message {
-            Message::None => Task::none(),
-            Message::Quit => iced::exit(),
-        }
-    }
-
-    fn view(&self) -> iced::Element<'_, Message> {
-        let menu_from_items = |items| Menu::new(items).max_width(180.0).offset(0.0).spacing(5.0);
-
-        #[rustfmt::skip]
-        let menu_bar = menu_bar!(
-            (base_button("File", Message::None), {
-                menu_from_items(menu_items!(
-                    (labeled_button("Quit", Message::Quit))
-                ))
-            })
-        )
-        .draw_path(menu::DrawPath::Backdrop);
-
-        let layout = iced::widget::column![menu_bar];
-
-        container(scrollable(layout))
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .into()
-    }
-
-    fn theme(&self) -> Theme {
-        self.theme.clone()
-    }
-}
-
-fn base_button(
-    label: &str,
-    msg: Message,
-) -> button::Button<'_, Message, iced::Theme, iced::Renderer> {
-    Button::new(text(label).align_y(alignment::Vertical::Center)).on_press(msg)
-}
-
-fn labeled_button(
-    label: &str,
-    msg: Message,
-) -> button::Button<'_, Message, iced::Theme, iced::Renderer> {
-    base_button(label, msg).width(Length::Fill)
+    // Starts the application's event loop with `()` as the application's flags.
+    cosmic::app::run::<app::AppModel>(settings, ())
 }
